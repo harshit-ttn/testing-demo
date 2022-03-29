39a27b5b365434370593a146842212ccfb6aa09b
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-30 00:24:11 +0530

    Testing Strings

--- test/lib.test.js
+++ test/lib.test.js
@@ -17,3 +17,13 @@ describe('absolute',()=>{
     });
 });
 
+
+
+describe('greet', () => {
+    it('should return the greeting message',() =>{
+        const result = lib.greet('Mosh');
+        expect(result).toMatch(/Mosh/);
+        expect(result).toContain('Mosh');
+
+    });
+});
