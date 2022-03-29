1834e2bb8348435d14fc4b30bd4c6b530a3fccda
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-30 02:38:47 +0530

    Testing Objects

--- test/lib.test.js
+++ test/lib.test.js
@@ -52,3 +52,14 @@ describe('getCurrencies', () => {
     })
 });
 
+
+describe('getProduct',() => {
+    it('should return the product with the given id',()=>{
+        const result = lib.getProduct(1);
+        // expect(result).toEqual({ id: 1, price: 10});
+        
+        expect(result).toMatchObject({ id: 1, price:10 });
+
+        expect(result).toHaveProperty('id',1);
+    });
+});
