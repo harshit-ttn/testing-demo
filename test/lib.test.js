fc19d9c221f2a5312c9be8a9d8a4bdd158299ec0
harshit-ttn <99420919+harshit-ttn@users.noreply.github.com>
2022-03-30 12:22:03 +0530

    Testing Exceptions

--- test/lib.test.js
+++ test/lib.test.js
@@ -57,9 +57,34 @@ describe('getProduct',() => {
     it('should return the product with the given id',()=>{
         const result = lib.getProduct(1);
         // expect(result).toEqual({ id: 1, price: 10});
-        
+
         expect(result).toMatchObject({ id: 1, price:10 });
 
         expect(result).toHaveProperty('id',1);
     });
+});
+
+
+describe('registerUser',() => {
+    it('should throw if username is falsy',() => {
+        // Null
+        // undefined
+        // NaN
+        // ''
+        // 0
+        // false
+        const args = [null,undefined,NaN,'',0,false];
+
+        args.forEach(a => {
+            expect(() => {lib.registerUser(a)}).toThrow();
+        });
+    });
+
+    it('should return a user object if valid username is passed',() =>{
+
+        const result = lib.registerUser('Harshit')
+        expect(result).toMatchObject( { username: 'Harshit'} );
+        expect(result.id).toBeGreaterThan(0);
+    });
+
 });
